f8c5a5564497df9887137af518ea4a6871ed3f27
dhandynuzirwan <169055100+dhandynuzirwan@users.noreply.github.com>
2026-06-06 23:10:23 +0700

    Penambahan Dialog seluruh babak, dan cutscene prolog

---
+++ scenes/maps/prolog.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _ready() -> void:
+	# 1. Jalankan timeline babak 1 secara otomatis
+	Dialogic.start("babak1_prolog")
+	
+	# 2. Perintahkan sistem: "Kalau dialognya tamat, jalankan fungsi pindah_scene"
+	Dialogic.timeline_ended.connect(_on_prolog_selesai)
+
+func _on_prolog_selesai() -> void:
+	# 3. Pindahkan pemain ke map top-down saat layar sudah hitam
+	# Pastikan path ini mengarah ke map kamar MC/test_room Anda!
+	get_tree().change_scene_to_file("res://scenes/maps/test_room.tscn")
